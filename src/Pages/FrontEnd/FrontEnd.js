4d32d8d40245a5c76d59ba3a8c9d99e1d049fcd3
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-22 18:02:30 +0000

    :sparkles: feat: Seção Talks C FrontEnd

---
+++ src/Pages/FrontEnd/FrontEnd.js
@@ -0,0 +1,13 @@
+import {View} from "react-native";
+import ListaTalks from "../../components/ListaTalks/ListaTalks";
+
+
+const FrontEnd = () => {
+    return (
+        <View>
+            <ListaTalks room="frontend" />
+        </View>
+    );
+};
+
+export default FrontEnd;
