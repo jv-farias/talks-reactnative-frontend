2dc70a39b8fe3cb72429663d375b70c5f775c946
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-22 18:00:40 +0000

    :boom: fix: Ajustando coo mponente Lista Tlks com props

--- src/components/ListaTalks/ListaTalks.js
+++ src/components/ListaTalks/ListaTalks.js
@@ -1,90 +1,63 @@
-import { ScrollView, TextInput, View, StyleSheet, SafeAreaView } from "react-native";
+import { ScrollView, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import CardPalestrante from "../CardPalestrante/CardPalestrante";
 import AsyncStorage from 'react-native';
 
+/**
+ * @typedef {Object} ListaTalksProps
+ * @property {string} [ListaTalksProps.room] - trilha do palestrante, pode ser: `principal`, `invite`, `frontend` ou `communities`
+ * @param {ListaTalksProps} props
+ */
 
-const ListaTalks = () => {
+const ListaTalks = ({ room }) => {
 
-    const [talks, setTalks] = useState({ principal: [], communities: [], frontend: [], invite: [] });
+    const roomMappings = {
+        'principal': 'Auditório',
+        'invite': 'Convida',
+        'frontend': 'Front-End',
+        'communities': 'Comunidades',
+    };
 
-
+    const [talks, setTalks] = useState([]);
 
     useEffect(() => {
         fetch('https://frontendday.descompliqueapps.com.br/index.php/wp-json/site/v1/data')
-            .then((response) => response.json())
-            .then((json) => {
-                setTalks(json.talks);
+            .then((resp) => resp.json())
+            .then((data) => {
+
+
+                if (room) {
+                    filteredTalks = data.talks[room];
+                } else {
+                    filteredTalks = [...data.talks.principal, ...data.talks.communities, ...data.talks.frontend, ...data.talks.invite];
+                }
+
+                // método usado para organizar as palestras pelo horário
+                const sortedTalks = filteredTalks.sort((a, b) => a.hour.localeCompare(b.hour));
+
+                setTalks(sortedTalks);
             });
-    }, []);
-
-
-
-
-
+    }, [room]);
     return (
 
-            <ScrollView  >
-                <View >
-                    {talks.principal.map((talk) =>
-                        <View key={talk.id}>
-                            <CardPalestrante
-                                socialLink={talk.speaker.social_link}
-                                horario={talk.hour}
-                                linkImg={talk.speaker.image}
-                                assunto={talk.title}
-                                cargo={`${talk.speaker.role} ${talk.speaker.company}`}
-                                nome={talk.speaker.title}
-                                trilha={talk.room}
-                            />
-                        </View>
-                    )}
-                    {talks.frontend.map((talk) =>
-                        <View key={talk.id}>
-                            <CardPalestrante
-                                socialLink={talk.speaker.social_link}
-                                horario={talk.hour}
-                                linkImg={talk.speaker.image}
-                                assunto={talk.title}
-                                cargo={`${talk.speaker.role} ${talk.speaker.company}`}
-                                nome={talk.speaker.title}
-                                trilha={talk.room}
-                            />
-                        </View>
-                    )}
-                    {talks.invite.map((talk) =>
-                        <View key={talk.id}>
-                            <CardPalestrante
-                                socialLink={talk.speaker.social_link}
-                                horario={talk.hour}
-                                linkImg={talk.speaker.image}
-                                assunto={talk.title}
-                                cargo={`${talk.speaker.role} ${talk.speaker.company}`}
-                                nome={talk.speaker.title}
-                                trilha={talk.room}
-                            />
-                        </View>
-                    )}
-                    {talks.communities.map((talk) =>
-                        <View key={talk.id}>
-                            <CardPalestrante
-                                socialLink={talk.speaker.social_link}
-                                horario={talk.hour}
-                                linkImg={talk.speaker.image}
-                                assunto={talk.title}
-                                cargo={`${talk.speaker.role} ${talk.speaker.company}`}
-                                nome={talk.speaker.title}
-                                trilha={talk.room}
-                            />
-                        </View>
-                    )}
-                </View>
-            </ScrollView>
-
-
+        <ScrollView  >
+            <View >
+                {talks.map((talk) =>
+                    <View key={talk.id}>
+                        <CardPalestrante
+                            socialLink={talk.speaker.social_link}
+                            horario={talk.hour}
+                            linkImg={talk.speaker.image}
+                            assunto={talk.title}
+                            cargo={`${talk.speaker.role} ${talk.speaker.company}`}
+                            nome={talk.speaker.title}
+                            trilha={roomMappings[talk.room]}
+                        />
+                    </View>
+                )}
+            </View>
+        </ScrollView>
     );
-
-
 }
 
 
